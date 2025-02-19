fix deepseek load hessian

vptq/models/deepseek.py
@@ -40,16 +40,35 @@ def get_deepseek(model_name, config_path, enable_offload=False):
     
     print(f'deepseek config: {model_args}')
 
-    # load model
-    model = Transformer(model_args)
-    
     # if seqlen is not None:
     #     model.seqlen = seqlen
         
     if enable_offload:
-        print(f"offload model load")
+        # load layer list
+        # layers = {}
+        layers = []
+        layer_files = sorted([f for f in os.listdir(model_name) if f.startswith('layer') and f.endswith('.pt')])
+        for layer_file in layer_files:
+            layer_idx = int(layer_file.split('_')[-1].split('.')[0])
+            layer_path = os.path.join(model_name, layer_file)
+            # layers[layer_idx] = layer_path
+            layers.append(layer_path)
+        
+        print(f'get offload layers: {layers}')
+        # load model
+        class OffloadModel(nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.model = None
+                self.layers = layers
+                self.model_args = model_args
+            def forward(self, x):
+                return x
+        print(f"offload model loaded")
+        model = OffloadModel()
     else:
         with torch.device("cpu"):  # Always load model to CPU first
+            model = Transformer(model_args)
             # Always load distributed model files
             start_time = time.time()
             path = os.path.join(model_name, f"model0-mp1.safetensors")
@@ -115,17 +134,33 @@ def quant_deepseek(model, args, quant_args, dev='cuda'):
     input_queues = mq_manager.Queue()
     output_queues = mq_manager.Queue()
 
-    # name to hessian
+    # rename to hessian
     name2hessian = {
         'attn.wq_a': 'attn.wq_a',
         'attn.wq_b': 'attn.wq_b',
         'attn.wkv_a': 'attn.wkv_a',
         'attn.wkv_b': 'attn.wkv_b',
         'attn.wo': 'attn.wo',
-        'mlp.w1': 'mlp.w1',
-        'mlp.w2': 'mlp.w2',
-        'mlp.w3': 'mlp.w3',
+        'ffn.w1': 'ffn.w1',
+        'ffn.w2': 'ffn.w2',
+        'ffn.w3': 'ffn.w1',
     }
+
+    # init MoE name to hessian, shared_experts=1 for DeepSeek-V3
+    if model.model_args.n_shared_experts == 1:
+        name2hessian[f'ffn.shared_experts.w1'] = f'ffn.shared_experts.w1'
+        name2hessian[f'ffn.shared_experts.w2'] = f'ffn.shared_experts.w2'
+        name2hessian[f'ffn.shared_experts.w3'] = f'ffn.shared_experts.w1'
+    else:
+        for i in range(model.model_args.n_shared_experts):
+            name2hessian[f'ffn.shared_experts.{i}.w1'] = f'ffn.shared_experts.{i}.w1'
+            name2hessian[f'ffn.shared_experts.{i}.w2'] = f'ffn.shared_experts.{i}.w2'
+            name2hessian[f'ffn.shared_experts.{i}.w3'] = f'ffn.shared_experts.{i}.w1'
+    
+    for i in range(model.model_args.n_routed_experts):
+        name2hessian[f'ffn.experts.{i}.w1'] = f'ffn.experts.{i}.w1'
+        name2hessian[f'ffn.experts.{i}.w2'] = f'ffn.experts.{i}.w2'
+        name2hessian[f'ffn.experts.{i}.w3'] = f'ffn.experts.{i}.w1'
     
     target_layers = [ColumnParallelLinear, RowParallelLinear, Linear]
  
@@ -197,7 +232,7 @@ def quant_deepseek(model, args, quant_args, dev='cuda'):
         print('Error: not all layers are quantized')
         exit(1)
 
-    qmodel = get_quantized_llama(model_name, args.seq_len, layer_state_dicts, layer_qlinear_args)
+    qmodel = get_quantized_deepseek(model_name, args.seq_len, layer_state_dicts, layer_qlinear_args)
 
     model = qmodel
 
@@ -207,12 +242,12 @@ def quant_deepseek(model, args, quant_args, dev='cuda'):
     return model, quantizers
 
 
-def get_quantized_llama(model_name, seqlen, layer_state_dicts, layer_qlinear_args):
+def get_quantized_deepseek(model_name, seqlen, layer_state_dicts, layer_qlinear_args):
 
     # print(f'layer_state_dicts: {layer_state_dicts.keys()}')
     model = get_data(model_name, seqlen=seqlen)
     dtype = next(iter(model.parameters())).dtype
-    layers = model.model.layers
+    layers = model.layers
 
     for layer_idx, layer_state_dict in layer_state_dicts.items():
         # print(f'load quantized layer {layer_idx}')

vptq/quantize_executer.py
@@ -61,7 +61,21 @@ def quantize_executer(task_id, tasks, args, quant_args, input_queues, output_que
         layer_qlinear_args = {}
 
     for (layer_idx, layer) in tasks:
-        dtype = next(iter(layer.parameters())).dtype
+        # offload layer
+        if args.enable_offload:
+            layer = torch.load(layer)
+            print(f'load layer {layer_idx} from {layer}')
+        
+        # fix dtype for fp8
+        if quant_args.cast_dtype is not None:
+            if quant_args.cast_dtype == 'bf16':
+                dtype = torch.bfloat16
+            elif quant_args.cast_dtype == 'fp16':
+                dtype = torch.float16
+            else:
+                raise ValueError(f'Invalid cast dtype: {quant_args.cast_dtype}')
+        else:
+            dtype = next(iter(layer.parameters())).dtype
 
         current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
         logger.info(f'----Quantizing layer {layer_idx} ...---- {current_time} on {dev} dtype {dtype}')

vptq/quantizer.py
@@ -36,38 +36,8 @@ class QuantizationArguments:
     enable_norm: bool = field(default=False)
     norm_dim: int = field(default=0, metadata={"help": "0: norm out feature, , 1: norm in feature"})
     enable_perm: bool = field(default=False)
-    # config_scale: Literal['minmax', 'np.nanstd',adamaxax'] = field(
-    #     default='minmax',
-    #     metadata={
-    #         "help": "Scaling method for quantization: "
-    #                "minmax (min-max scaling), "
-    #                "meanstd (mean-std normalization), "
-    #                "absmax (absolute max scaling)"
-    #     }
-    # )
-    # centroid_dtype: str = field(
-    #     default='int8',
-    #     metadata={
-    #         "help": "Data type for centroids. Options: fp16, bf16, fp8, int8",
-    #         "dtype_mapping": {
-    #             "fp16": torch.float16, 
-    #             "bf16": torch.bfloat16,
-    #             "fp8": torch.float8_e4m3fn,
-    #             "int8": torch.int8,
-    #         }
-    #     }
-    # )
-    # scale_dtype: str = field(
-    #     default='fp16',
-    #     metadata={
-    #         "help": "Data type for scale. Options: fp16, bf16, fp8",
-    #         "dtype_mapping": {
-    #             "fp16": torch.float16, 
-    #             "bf16": torch.bfloat16,
-    #             "fp8": torch.float8_e4m3fn,
-    #         }
-    #     }
-    # )
+    cast_dtype: str = field(default=None)
+
 
 # N-percent outlier Vector Quantizator
 # Partition data into N% outliers and (100-N)%.